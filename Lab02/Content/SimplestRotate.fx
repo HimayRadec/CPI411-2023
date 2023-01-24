42da2894abb57b496e7f78c7e1ee8780061e31f8
Himay <himayradec@gmail.com>
2023-01-24 09:43:28 -0700

    Lab 2 Complete

--- Lab02/Content/SimplestRotate.fx
+++ Lab02/Content/SimplestRotate.fx
@@ -1,4 +1,9 @@
-﻿texture MyTexture;
+texture MyTexture;
+
+float3 offset;
+float4x4 World;
+float4x4 View;
+float4x4 Projection;
 
 sampler mySampler = sampler_state {
 	Texture = < MyTexture>;
@@ -10,7 +15,14 @@ struct VertexPositionTexture {
 };
 
 VertexPositionTexture MyVertexShader(VertexPositionTexture input) {
-	return input;
+	//input.position.xyz += offset;
+	VertexPositionTexture output;
+	float4 worldPos = mul(input.position, World);
+	float4 viewPos = mul(worldPos, View);
+	output.position = mul(viewPos, Projection);
+	output.textureCoordinate = input.textureCoordinate;
+
+	return output;
 }
 
 float4 MyPixelShader(VertexPositionTexture input) : COLOR
--- Lab02/Lab02.cs
+++ Lab02/Lab02.cs
@@ -9,11 +9,28 @@ namespace Lab02
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        // My Code
+        Effect effect;
+        VertexPositionTexture[] vertices =
+        {
+            new VertexPositionTexture(new Vector3(0, 1, 0), new Vector2(0.5f,0)),
+            new VertexPositionTexture(new Vector3(1, 0, 0), new Vector2(1,1)),
+            new VertexPositionTexture(new Vector3(-1, 0, 0), new Vector2(0,1))
+        };
+
+        float angle;
+        float distance = 1;
+        Matrix view;
+        Matrix world;
+        Matrix projection;
+
         public Lab02()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+
+            _graphics.GraphicsProfile = GraphicsProfile.HiDef;
         }
 
         protected override void Initialize()
@@ -28,6 +45,8 @@ namespace Lab02
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            effect = Content.Load<Effect>("SimplestRotate");
+            effect.Parameters["MyTexture"].SetValue(Content.Load<Texture2D>("logo_mg"));
         }
 
         protected override void Update(GameTime gameTime)
@@ -37,6 +56,49 @@ namespace Lab02
 
             // TODO: Add your update logic here
 
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                angle += 0.02f;
+                Vector3 offset = new Vector3(
+                    (float)System.Math.Cos(angle),
+                    (float)System.Math.Sin(angle),
+                    0);
+                effect.Parameters["offset"].SetValue(offset);
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                angle -= 0.02f;
+                Vector3 offset = new Vector3(
+                    (float)System.Math.Cos(angle),
+                    (float)System.Math.Sin(angle),
+                    0);
+                effect.Parameters["offset"].SetValue(offset);
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+                distance -= 0.02f;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Down))
+            {
+                distance += 0.02f;
+            }
+
+            Matrix world = Matrix.Identity;
+            Matrix view = Matrix.CreateLookAt(new Vector3(angle, 0, distance),new Vector3(),new Vector3(0, 1, 0));
+            Matrix projection = Matrix.CreatePerspectiveFieldOfView(
+                MathHelper.ToRadians(90),
+                GraphicsDevice.Viewport.AspectRatio,
+                0.1f, 100);
+
+       
+
+            effect.Parameters["World"].SetValue(world);
+            effect.Parameters["View"].SetValue(view);
+            effect.Parameters["Projection"].SetValue(projection);
+
             base.Update(gameTime);
         }
 
@@ -44,7 +106,18 @@ namespace Lab02
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
+            //Allows Opacity/Blending?
+
+            GraphicsDevice.BlendState = BlendState.AlphaBlend;
+
             // TODO: Add your drawing code here
+            foreach (var pass in effect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+                GraphicsDevice.DrawUserPrimitives<VertexPositionTexture>(
+                //GraphicsDevice.DrawUserPrimitives<VertexPositionColor>(
+                  PrimitiveType.TriangleList, vertices, 0, vertices.Length / 3);
+            }
 
             base.Draw(gameTime);
         }
