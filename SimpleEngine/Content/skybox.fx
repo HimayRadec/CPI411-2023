87aaca6814ab63129a4ec48a2494ba6d03ee351c
HimayRadec <80723658+HimayRadec@users.noreply.github.com>
2023-03-17 14:43:31 -0700

    Skybox

---
+++ SimpleEngine/Content/skybox.fx
@@ -0,0 +1,53 @@
+﻿float4x4 World;
+float4x4 View;
+float4x4 Projection;
+float3 CameraPosition;
+Texture SkyBoxTexture;
+
+samplerCUBE SkyBoxSampler = sampler_state
+{
+	texture = <SkyBoxTexture>;
+	magfilter = LINEAR; //Linear interpolation filter
+	minfilter = LINEAR;
+	mipfilter = LINEAR;
+
+	AddressU = Mirror;
+	AddressV = Mirror;
+};
+
+struct VertexShaderInput {
+	float4 Position: POSITION0;
+};
+
+struct VertexShaderOutput {
+	float4 Position: POSITION0;
+	float3 TextureCoordinate: TEXCOORD0;
+};
+
+VertexShaderOutput MyVertexShader(VertexShaderInput input)
+{
+	VertexShaderOutput output;
+
+
+	float4 worldPos = mul(input.Position, World);
+	float4 viewPos = mul(worldPos, View);
+	output.Position = mul(viewPos, Projection);
+	output.TextureCoordinate = worldPos.xyz - CameraPosition;
+
+	return output;
+}
+
+float4 MyPixelShader(VertexShaderOutput input) : COLOR
+{
+
+	return texCUBE(SkyBoxSampler, normalize(input.TextureCoordinate));
+}
+
+technique MyTechnique
+{
+	pass Pass1
+	{
+		VertexShader = compile vs_4_0 MyVertexShader();
+		PixelShader = compile ps_4_0 MyPixelShader();
+	}
+}
--- SimpleEngine/Skybox.cs
+++ SimpleEngine/Skybox.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Content;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace CPI411.SimpleEngine
 {
@@ -9,17 +10,24 @@ namespace CPI411.SimpleEngine
         private Model skyBox;
         public TextureCube skyBoxTexture;
         private Effect skyBoxEffect;
-        private float size = 50f;
+        private float size = 50f; //Can make larger if I want
 
-        public Skybox(string[] skyboxTextures, ContentManager Content, GraphicsDevice g)
+
+        //Constructor
+        public Skybox(string[] skyboxTextures, int dim, ContentManager Content, GraphicsDevice g)
         {
-            skyBox = Content.Load<Model>("skybox/cube");
-            // "skybox/Skybox
-            skyBoxEffect = Content.Load<Effect>("skybox/Skybox");
-            skyBoxTexture = new TextureCube(g, 512, false, SurfaceFormat.Color);
-            byte[] data = new byte[512 * 512 * 4];
+            //Make these arguments part of constructor in the future to make it reusible.
+            skyBox = Content.Load<Model>("cube"); //"skybox/cube" use sub-folders!
+            skyBoxEffect = Content.Load<Effect>("skybox");
+
+
+
+            skyBoxTexture = new TextureCube(g, dim, false, SurfaceFormat.Color);
+            byte[] data = new byte[dim * dim * 4]; //512 x 514 for the image, x4 for rgba data.
+
+            //This assigns all 6 faces of the skybox.
             Texture2D tempTexture = Content.Load<Texture2D>(skyboxTextures[0]);
-            tempTexture.GetData<byte>(data);
+            tempTexture.GetData<byte>(data); //All pixel data is stored into byte array data
             skyBoxTexture.SetData<byte>(CubeMapFace.NegativeX, data);
 
             tempTexture = Content.Load<Texture2D>(skyboxTextures[1]);
@@ -43,7 +51,6 @@ namespace CPI411.SimpleEngine
             skyBoxTexture.SetData<byte>(CubeMapFace.PositiveZ, data);
 
         }
-
         public void Draw(Matrix view, Matrix projection, Vector3 cameraPosition)
         {
             foreach (EffectPass pass in skyBoxEffect.CurrentTechnique.Passes)
@@ -55,16 +62,18 @@ namespace CPI411.SimpleEngine
                     {
                         part.Effect = skyBoxEffect;
                         part.Effect.Parameters["World"].SetValue(
-                        Matrix.CreateScale(size) *
-                        Matrix.CreateTranslation(cameraPosition));
+                            Matrix.CreateScale(size) *
+                            Matrix.CreateTranslation(cameraPosition));
+
                         part.Effect.Parameters["View"].SetValue(view);
                         part.Effect.Parameters["Projection"].SetValue(projection);
                         part.Effect.Parameters["SkyBoxTexture"].SetValue(skyBoxTexture);
                         part.Effect.Parameters["CameraPosition"].SetValue(cameraPosition);
                     }
-                    mesh.Draw();
+                    mesh.Draw(); //Built-in draw method for monogame.
                 }
             }
         }
     }
 }
+    
