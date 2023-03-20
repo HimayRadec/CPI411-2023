08e9f4df6acd0ba1597f0fda8a1123a12549b6b7
HimayRadec <80723658+HimayRadec@users.noreply.github.com>
2023-03-19 19:05:22 -0700

    Error on Lab 07

--- Lab07/Content/BumpMap.fx
+++ Lab07/Content/BumpMap.fx
@@ -1,4 +1,6 @@
-﻿float4x4 World;
+﻿// *** CPI411 Lab#7 (BumpMap) 
+
+float4x4 World;
 float4x4 View;
 float4x4 Projection;
 float4x4 WorldInverseTranspose;
@@ -6,13 +8,20 @@ float4x4 WorldInverseTranspose;
 float3 CameraPosition;
 float3 LightPosition;
 
-float4 AmbientColor;
+// Light Uniforms
+float AmbientColor;
 float AmbientIntensity;
+
 float4 DiffuseColor;
 float DiffuseIntensity;
+
 float4 SpecularColor;
 float SpecularIntensity;
 float Shininess;
+
+// Bump Mapping Uniforms
+float height = 1.0f;
+float2 UVScale = float2(1.0f, 1.0f);
 
 texture normalMap;
 
@@ -25,69 +34,109 @@ sampler tsampler1 = sampler_state {
 	AddressV = Wrap;
 };
 
-struct VertexInput
+bool useSelfShadowing = false;
+
+struct VertexShaderInput
 {
-	float4 Position: POSITION;
-	float2 TexCoord: TEXCOORD;
-	float4 Normal: NORMAL;
-	float4 Tangent: TANGENT0;
-	float4 Binormal: BINORMAL0;
+	float4 Position : POSITION0;
+	float4 Normal : NORMAL0;
+	float4 Tangent : TANGENT0;
+	float4 Binormal : BINORMAL0;
+	float2 TexCoord : TEXCOORD0;
 };
 
 struct VertexShaderOutput
 {
-	float4 Position: POSITION;
-	float3 Normal: TEXCOORD0;
-	float3 Tangent: TEXCOORD1;
-	float3 Binormal: TEXCOORD2;
-	float2 TexCoord: TEXCOORD3;
-	float3 Position3D: TEXCOORD4; // World Position
-
+	float4 Position : POSITION0;
+	float3 Normal : TEXCOORD0;
+	float3 Tangent : TEXCOORD1;
+	float3 Binormal : TEXCOORD2;
+	float2 TexCoord : TEXCOORD3;
+	float3 Position3D : TEXCOORD4;
 };
 
-VertexShaderOutput BumpMapVertexShader(VertexInput input)
+VertexShaderOutput VertexShaderFunction(VertexShaderInput input)
 {
 	VertexShaderOutput output;
+
 	float4 worldPosition = mul(input.Position, World);
 	float4 viewPosition = mul(worldPosition, View);
 	output.Position = mul(viewPosition, Projection);
-	output.Position3D = worldPosition.xyz;
+
+	// TODO: add your vertex shader code here.
+	/* // to transform from object space to tangent space
+	float3x3 objectToTangentSpace;
+	objectToTangentSpace[0] = input.Tangent;
+	objectToTangentSpace[1] = input.Binormal;
+	objectToTangentSpace[2] = input.Normal;
+	// [ Tx, Ty, Tz ] [ Objx ] = [ Tanx ]
+	// [ Bx, By, Bz ] [ Objy ] = [ Tany ]
+	// [ Nx, Ny, Nz ] [ Objz ] = [ Tanz ]
+
+	output.Normal = mul(objectToTangentSpace, input.Normal); // object -> tangent?
+	output.Tangent = mul(objectToTangentSpace, input.Tangent);
+	output.Binormal = mul(objectToTangentSpace, input.Binormal);
+	//*/
+
+	// World Space looks better IMO
 	output.Normal = normalize(mul(input.Normal, WorldInverseTranspose).xyz);
 	output.Tangent = normalize(mul(input.Tangent, WorldInverseTranspose).xyz);
 	output.Binormal = normalize(mul(input.Binormal, WorldInverseTranspose).xyz);
-	output.TexCoord = input.TexCoord;
 
+	output.Position3D = worldPosition.xyz;
+	output.TexCoord = input.TexCoord;//* UVScale;
 	return output;
 }
 
-float4 BumpMapPixelShader(VertexShaderOutput input) : COLOR0
+float4 PixelShaderFunction(VertexShaderOutput input) : COLOR0
 {
-	float3 N = input.Normal;
-	float3 V = normalize(CameraPosition - input.Position3D.xyz);
-	float3 L = normalize(LightPosition - input.Position3D.xyz);
-	float3 R = reflect(-L, N);
 
-	float3 T = input.Tangent;
-	float3 B = input.Binormal;
+	// Vectors: L = Light; V = View; N = Normal; T = Tangent; B = Binormal; H = Halfway between L and V
+	float3 L = normalize(LightPosition - input.Position3D);
+	float3 V = normalize(CameraPosition - input.Position3D);
+	float3 N = normalize(input.Normal);
+	float3 T = normalize(input.Tangent);
+	float3 B = normalize(input.Binormal);
 	float3 H = normalize(L + V);
 
-	float3 normalTex = (tex2D(tsampler1, input.TexCoord).xyz - float3(0.5, 0.5, 0.5)) * 2.0;
+	// Calculate the normal, including the information in the bump map
+	float3 normalTex = tex2D(tsampler1, input.TexCoord).xyz;
+	normalTex = 1.0 * 2.0 * (normalTex - float3(0.5, 0.5, 0.5)); //expand(normalTex);
 
-	float3 bumpNormal = N + normalTex.x* T + normalTex.y * B;
+	// *** Lab7 ********
+	//float3 bumpNormal = normalize(N + (normalTex.x * T + normalTex.y * B));
 
-	float4 ambient = AmbientColor * AmbientIntensity;
-	float4 diffuse = DiffuseIntensity * DiffuseColor * max(0, dot(bumpNormal, L));
-	float4 specular = pow(max(0, dot(H, bumpNormal)), Shininess) * SpecularColor * SpecularIntensity;
-	float4 color = saturate(ambient + diffuse + specular);
+	// (Lab7 Option MonoGame3.4) 
+	// If does not work, use the OPTION-A
+	//float3 bumpNormal = normalize(N + (normalTex.x * float3(1, 0, 0) + normalTex.y * float3(0, 1, 0))); // OPTION A
+	
+	// *** for Assignment3 ***
+	float3x3 TangentToWorld;
 
-	return color;
+	TangentToWorld[0] = (input.Tangent);
+	TangentToWorld[1] = (input.Binormal);
+	TangentToWorld[2] = (input.Normal);
+
+	float3 bumpNormal = mul(normalTex, TangentToWorld);
+
+	//calculate Diffuse Term:
+	float4 diffuse = DiffuseColor * DiffuseIntensity * max(0, (dot(bumpNormal, L)));
+	diffuse.a = 1.0;
+
+	// calculate Specular Term (H,N):
+	float4 specular = SpecularColor * SpecularIntensity * pow(saturate(dot(H, bumpNormal)), Shininess);
+	specular.a = 1.0;
+
+	// Compute Final Color
+	float4 finalColor = diffuse + specular; //ambient + diffuse + specular;
+	return finalColor;
 }
 
-technique BumpMap
+technique Technique1
 {
 	pass Pass1
 	{
-		VertexShader = compile vs_4_0 BumpMapVertexShader();
-		PixelShader = compile ps_4_0 BumpMapPixelShader();
+		VertexShader = compile vs_4_0 VertexShaderFunction();
+		PixelShader = compile ps_4_0 PixelShaderFunction();
 	}
-};
+}
--- Lab07/Lab07.cs
+++ Lab07/Lab07.cs
@@ -6,141 +6,112 @@ namespace Lab07
 {
     public class Lab07 : Game
     {
-        private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        SpriteFont font;
+        Effect effect;
+        //Skybox skybox;
+        Matrix world = Matrix.Identity;
+        Matrix view = Matrix.CreateLookAt(new Vector3(20, 0, 0), new Vector3(0, 0, 0), Vector3.UnitY);
+        Matrix projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45), 800f / 600f, 0.1f, 100f);
+        Vector3 cameraPosition;
+        Vector3 lightPosition;
 
-        // Content
+        float angle = 0;
+        float angle2 = 0;
+        float angleL = 0;
+        float angleL2 = 0;
+        float distance = 30;
+        MouseState preMouse;
+
+        Vector3 viewVector;
+        Vector3 cameraTarget = new Vector3(0, 0, 0);
+
         Model model;
         Texture2D texture;
-        Effect effect;
-
-        // Camera
-        float angle;            // X Rotation
-        float angle2;           // Y Rotation
-        float distance = 10f;
-        Vector3 cameraPosition;
-        Vector3 lightPosition = new Vector3(100, 100, 100);
-        Matrix world;
-        Matrix view;
-        Matrix projection;
-        MouseState preMouseState;
-
-        // Lighting Angles
-        private float angleL;
-        private float angleL2;
-
-        // Move Camera
-        private float cameraTarget;
 
         public Lab07()
         {
-            _graphics = new GraphicsDeviceManager(this);
+            graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-            IsMouseVisible = true;
-
-            _graphics.GraphicsProfile = GraphicsProfile.HiDef;
+            // ***** From MonoGame3.6 Need this statement
+            graphics.GraphicsProfile = GraphicsProfile.HiDef;
+            // **********************************************
         }
-
         protected override void Initialize()
         {
-
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
-            _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            // font = Content.Load<SpriteFont>("font");
             model = Content.Load<Model>("Plane");
-            texture = Content.Load<Texture2D>("NormalMap/round");
             effect = Content.Load<Effect>("BumpMap");
+            texture = Content.Load<Texture2D>("NormalMap/round");
         }
+
+        protected override void UnloadContent() { }
 
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-
-            #region - Camera Movement -
-
-            MouseState currentMouseState = Mouse.GetState();
-
-            // Rotate Camera
-            if (currentMouseState.LeftButton == ButtonState.Pressed && 
-                preMouseState.LeftButton == ButtonState.Pressed)
-            {
-                angle -= (preMouseState.X - currentMouseState.X) / 100f;
-                angle2 -= (preMouseState.Y - currentMouseState.Y) / 100f;
-            }
-            // Zoom In/Out
-            if (Mouse.GetState().RightButton == ButtonState.Pressed)
-            {
-                distance += (Mouse.GetState().X - preMouseState.X) / 100f;
-            }
-            /* TODO: Fix the cameraTarget semantic error.
-            if (Mouse.GetState().MiddleButton == ButtonState.Pressed)
-            {
-                Vector3 ViewRight = Vector3.Transform(Vector3.UnitX,
-                Matrix.CreateRotationX(angle2) *
-                Matrix.CreateRotationY(angle));
-                Vector3 ViewUp = Vector3.Transform(Vector3.UnitY,
-                Matrix.CreateRotationX(angle2) *
-                Matrix.CreateRotationY(angle));
-                cameraTarget -= ViewRight * ((Mouse.GetState().X - preMouseState.X) / 10f);
-                cameraTarget += ViewUp * ((Mouse.GetState().Y - preMouseState.Y) / 10f);
-            }
-            */
-            preMouseState = currentMouseState;
-
-            #endregion
-
-            #region - Lighting Angle -
+            cameraPosition = distance * new Vector3((float)System.Math.Sin(angle), 0, (float)System.Math.Cos(angle));
 
             if (Keyboard.GetState().IsKeyDown(Keys.Left)) angleL += 0.02f;
             if (Keyboard.GetState().IsKeyDown(Keys.Right)) angleL -= 0.02f;
             if (Keyboard.GetState().IsKeyDown(Keys.Up)) angleL2 += 0.02f;
             if (Keyboard.GetState().IsKeyDown(Keys.Down)) angleL2 -= 0.02f;
 
-            lightPosition = Vector3.Transform(new Vector3(0, 0, 10), Matrix.CreateRotationX(angleL2) * Matrix.CreateRotationY(angleL));
-
-            #endregion
-
-            #region - Matrix Transformations -
-
-            world = Matrix.Identity;
-            cameraPosition = Vector3.Transform(
-                new Vector3(0, 0, distance),
-                Matrix.CreateRotationX(angle2) * Matrix.CreateRotationY(angle)
+            if (Mouse.GetState().LeftButton == ButtonState.Pressed)
+            {
+                angle -= (Mouse.GetState().X - preMouse.X) / 100f;
+                angle2 += (Mouse.GetState().Y - preMouse.Y) / 100f;
+            }
+            if (Mouse.GetState().RightButton == ButtonState.Pressed)
+            {
+                distance += (Mouse.GetState().X - preMouse.X) / 100f;
+            }
+            if (Mouse.GetState().MiddleButton == ButtonState.Pressed)
+            {
+                Vector3 ViewRight = Vector3.Transform(Vector3.UnitX,
+                    Matrix.CreateRotationX(angle2) * Matrix.CreateRotationY(angle));
+                Vector3 ViewUp = Vector3.Transform(Vector3.UnitY,
+                    Matrix.CreateRotationX(angle2) * Matrix.CreateRotationY(angle));
+                cameraTarget -= ViewRight * (Mouse.GetState().X - preMouse.X) / 10f;
+                cameraTarget += ViewUp * (Mouse.GetState().Y - preMouse.Y) / 10f;
+            }
+            preMouse = Mouse.GetState();
+            // Update Camera
+            cameraPosition = Vector3.Transform(new Vector3(0, 0, distance),
+                Matrix.CreateRotationX(angle2) * Matrix.CreateRotationY(angle) * Matrix.CreateTranslation(cameraTarget)
                 );
-            view = Matrix.CreateLookAt(
-                cameraPosition,
-                new Vector3(),
-                Vector3.Transform(
-                    Vector3.Up,
-                    Matrix.CreateRotationX(angle2) * Matrix.CreateRotationY(angle))
-                );
-            projection = Matrix.CreatePerspectiveFieldOfView(
-                MathHelper.ToRadians(90),
-                1.33f,
-                0.1f, 
-                100
-                );
+            view = Matrix.CreateLookAt(cameraPosition, cameraTarget, Vector3.Transform(Vector3.UnitY,
+                    Matrix.CreateRotationX(angle2) * Matrix.CreateRotationY(angle)));
 
-            #endregion
+            // Update Light
+            lightPosition = Vector3.Transform(new Vector3(0, 0, 10),
+                Matrix.CreateRotationX(angleL2) * Matrix.CreateRotationY(angleL));
 
             base.Update(gameTime);
         }
 
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
-            //GraphicsDevice.BlendState = BlendState.Opaque;
-            //GraphicsDevice.DepthStencilState = new DepthStencilState();
+            GraphicsDevice.BlendState = BlendState.Opaque;
+            GraphicsDevice.DepthStencilState = new DepthStencilState();
 
             effect.CurrentTechnique = effect.Techniques[0];
+
             foreach (EffectPass pass in effect.CurrentTechnique.Passes)
             {
                 foreach (ModelMesh mesh in model.Meshes)
@@ -150,29 +121,31 @@ namespace Lab07
                         effect.Parameters["World"].SetValue(mesh.ParentBone.Transform);
                         effect.Parameters["View"].SetValue(view);
                         effect.Parameters["Projection"].SetValue(projection);
-                        effect.Parameters["CameraPosition"].SetValue(cameraPosition);
+                        Matrix worldInverseTransposeMatrix = Matrix.Transpose(Matrix.Invert(mesh.ParentBone.Transform));
+                        effect.Parameters["WorldInverseTranspose"].SetValue(worldInverseTransposeMatrix);
 
-                        Matrix worldInverseTranspose = Matrix.Transpose(Matrix.Invert(mesh.ParentBone.Transform));
-                        effect.Parameters["WorldInverseTranspose"].SetValue(worldInverseTranspose);
+                        effect.Parameters["CameraPosition"].SetValue(cameraPosition);
                         effect.Parameters["LightPosition"].SetValue(lightPosition);
-                        effect.Parameters["AmbientColor"].SetValue(new Vector4(1,1,1,1));
-                        effect.Parameters["AmbientIntensity"].SetValue(0);
-                        effect.Parameters["DiffuseColor"].SetValue(new Vector4(1, 1, 1, 1));
-                        effect.Parameters["DiffuseIntensity"].SetValue(1f);
-                        effect.Parameters["SpecularColor"].SetValue(new Vector4(1, 1, 1, 1));
-                        effect.Parameters["Shininess"].SetValue(20f);
+                        effect.Parameters["DiffuseIntensity"].SetValue(1.0f);
+                        effect.Parameters["DiffuseColor"].SetValue(new Vector4(1.0f, 1.0f, 1.0f, 1.0f));
+                        effect.Parameters["SpecularIntensity"].SetValue(1.0f);
+                        effect.Parameters["SpecularColor"].SetValue(new Vector4(1.0f, 1.0f, 1.0f, 1.0f));
+                        effect.Parameters["Shininess"].SetValue(100.0f);
+
                         effect.Parameters["normalMap"].SetValue(texture);
 
                         pass.Apply();
                         GraphicsDevice.SetVertexBuffer(part.VertexBuffer);
                         GraphicsDevice.Indices = part.IndexBuffer;
+
                         GraphicsDevice.DrawIndexedPrimitives(
-                        PrimitiveType.TriangleList, part.VertexOffset, part.StartIndex, part.PrimitiveCount);
+                            PrimitiveType.TriangleList,
+                            part.VertexOffset,
+                            part.StartIndex,
+                            part.PrimitiveCount);
                     }
                 }
             }
-
-            base.Draw(gameTime);
         }
     }
 }
