a35f10bbb3bb6d308e374977c5d5d9df9cadbd11
HimayRadec <80723658+HimayRadec@users.noreply.github.com>
2023-03-21 11:46:55 -0700

    Worked on Lab09

---
+++ Lab09/Lab09.cs
@@ -0,0 +1,225 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Lab09
+{
+    public class Lab09 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch spriteBatch;
+        // **** TEMPLATE ************//
+        SpriteFont font;
+        Effect effect;
+        Matrix world = Matrix.Identity;
+        Matrix view = Matrix.CreateLookAt(new Vector3(20, 0, 0), new Vector3(0, 0, 0), Vector3.UnitY);
+        Matrix projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45), 800f / 600f, 0.1f, 100f);
+        Vector3 cameraPosition, cameraTarget, lightPosition;
+        float angle = 0;
+        float angle2 = 0;
+        float angleL = 0;
+        float angleL2 = 0;
+        float distance = 30;
+        MouseState preMouse;
+        //Model model;
+        Model[] models;
+        Texture2D texture;
+
+        Matrix lightView = Matrix.CreateLookAt(new Vector3(0, 0, 10), Vector3.Zero, Vector3.UnitY);
+        Matrix lightProjection = Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver2, 1f, 1f, 100f);
+        Texture2D shadowMap;
+        // **** TEMPLATE ************//
+
+        RenderTarget2D renderTarget;
+
+        public Lab09()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            //spriteBatch = new SpriteBatch(GraphicsDevice);
+            font = Content.Load<SpriteFont>("font");
+            //model = Content.Load<Model>("Plane");
+            //effect = Content.Load<Effect>("ProjectiveTexture");
+            texture = Content.Load<Texture2D>("nvlobby_new_negz");
+            models = new Model[2];
+            models[0] = Content.Load<Model>("Plane");
+            models[1] = Content.Load<Model>("torus");
+            //shadowMap = Content.Load<Texture2D>()
+
+            // TODO: use this.Content to load your game content here
+
+            PresentationParameters pp = GraphicsDevice.PresentationParameters;
+            renderTarget = new RenderTarget2D(GraphicsDevice, 2048, 2048, false, SurfaceFormat.Single, DepthFormat.Depth24, 0, RenderTargetUsage.PlatformContents);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // ************ TEMPLATE ************ //
+            if (Keyboard.GetState().IsKeyDown(Keys.Left)) angleL += 0.02f;
+            if (Keyboard.GetState().IsKeyDown(Keys.Right)) angleL -= 0.02f;
+            if (Keyboard.GetState().IsKeyDown(Keys.Up)) angleL2 += 0.02f;
+            if (Keyboard.GetState().IsKeyDown(Keys.Down)) angleL2 -= 0.02f;
+            if (Keyboard.GetState().IsKeyDown(Keys.S)) { angle = angle2 = angleL = angleL2 = 0; distance = 30; cameraTarget = Vector3.Zero; }
+            if (Mouse.GetState().LeftButton == ButtonState.Pressed)
+            {
+                angle -= (Mouse.GetState().X - preMouse.X) / 100f;
+                angle2 += (Mouse.GetState().Y - preMouse.Y) / 100f;
+            }
+            if (Mouse.GetState().RightButton == ButtonState.Pressed)
+            {
+                distance += (Mouse.GetState().X - preMouse.X) / 100f;
+            }
+
+            if (Mouse.GetState().MiddleButton == ButtonState.Pressed)
+            {
+                Vector3 ViewRight = Vector3.Transform(Vector3.UnitX,
+                    Matrix.CreateRotationX(angle2) * Matrix.CreateRotationY(angle));
+                Vector3 ViewUp = Vector3.Transform(Vector3.UnitY,
+                    Matrix.CreateRotationX(angle2) * Matrix.CreateRotationY(angle));
+                cameraTarget -= ViewRight * (Mouse.GetState().X - preMouse.X) / 10f;
+                cameraTarget += ViewUp * (Mouse.GetState().Y - preMouse.Y) / 10f;
+            }
+            preMouse = Mouse.GetState();
+            // Update Camera
+            cameraPosition = Vector3.Transform(new Vector3(0, 0, distance),
+                Matrix.CreateRotationX(angle2) * Matrix.CreateRotationY(angle) * Matrix.CreateTranslation(cameraTarget));
+            view = Matrix.CreateLookAt(
+                cameraPosition,
+                cameraTarget,
+                Vector3.Transform(Vector3.UnitY, Matrix.CreateRotationX(angle2) * Matrix.CreateRotationY(angle)));
+
+
+            // ********************************** //
+
+
+
+            base.Update(gameTime);
+        }
+
+        private void DrawShadowMap()
+        {
+            foreach (Model model in models)
+            {
+                foreach (EffectPass pass in effect.CurrentTechnique.Passes)
+                {
+                    foreach (ModelMesh mesh in model.Meshes)
+                    {
+                        foreach (ModelMeshPart part in mesh.MeshParts)
+                        {
+
+                            effect.Parameters["World"].SetValue(mesh.ParentBone.Transform);
+                            effect.Parameters["LightViewMatrix"].SetValue(lightView);
+                            effect.Parameters["LightProjectionMatrix"].SetValue(lightProjection);
+
+
+                            pass.Apply();
+                            GraphicsDevice.SetVertexBuffer(part.VertexBuffer);
+                            GraphicsDevice.Indices = part.IndexBuffer;
+                            GraphicsDevice.DrawIndexedPrimitives(
+                                PrimitiveType.TriangleList,
+                                part.VertexOffset,
+                                part.StartIndex,
+                                part.PrimitiveCount);
+                        }
+                    }
+                }
+            }
+
+        }
+
+        private void DrawShadowedScene()
+        {
+            effect.CurrentTechnique = effect.Techniques["ShadowedScene"];
+            foreach (Model model in models)
+            {
+                foreach (EffectPass pass in effect.CurrentTechnique.Passes)
+                    foreach (ModelMesh mesh in model.Meshes)
+                    {
+                        foreach (ModelMeshPart part in mesh.MeshParts)
+                        {
+
+                            
+                            effect.Parameters["World"].SetValue(mesh.ParentBone.Transform);
+                            effect.Parameters["View"].SetValue(view);
+                            effect.Parameters["Projection"].SetValue(projection);
+                            Matrix worldInverseTransposeMatrix = Matrix.Transpose(Matrix.Invert(mesh.ParentBone.Transform));
+                            effect.Parameters["WorldInverseTranspose"].SetValue(worldInverseTransposeMatrix);
+                            effect.Parameters["LightViewMatrix"].SetValue(lightView);
+                            effect.Parameters["LightProjectionMatrix"].SetValue(lightProjection);
+                            effect.Parameters["LightPosition"].SetValue(lightPosition);
+                            effect.Parameters["CameraPosition"].SetValue(cameraPosition);
+                            effect.Parameters["ShadowMap"].SetValue(shadowMap);
+                            
+
+                            pass.Apply(); 
+                            GraphicsDevice.SetVertexBuffer(part.VertexBuffer);
+                            GraphicsDevice.Indices = part.IndexBuffer;
+                            GraphicsDevice.DrawIndexedPrimitives(
+                                PrimitiveType.TriangleList,
+                                part.VertexOffset,
+                                part.StartIndex,
+                                part.PrimitiveCount);
+                        }
+                    }
+            }
+        }
+
+    
+
+    protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // ************ TEMPLATE ************ //
+
+            GraphicsDevice.BlendState = BlendState.Opaque;
+            GraphicsDevice.DepthStencilState = new DepthStencilState();
+
+            // LAB 9: STEP 2
+            GraphicsDevice.SetRenderTarget(renderTarget);
+            GraphicsDevice.Clear(ClearOptions.Target | ClearOptions.DepthBuffer, Color.Black, 1.0f, 0);
+
+            // STEP 3
+            DrawShadowMap();
+
+            // STEP 4
+            GraphicsDevice.SetRenderTarget(null);
+            shadowMap = (Texture2D)renderTarget;
+
+            // *** Lab 9 : Step5: Clear the render target
+            GraphicsDevice.Clear(ClearOptions.Target |
+            ClearOptions.DepthBuffer, Color.DarkSlateBlue, 1.0f, 0);
+
+            using (SpriteBatch sprite = new SpriteBatch(GraphicsDevice))
+            {
+                sprite.Begin();
+                sprite.Draw(shadowMap, new Vector2(0, 0), null, Color.White, 0,
+                new Vector2(0, 0), 1f, SpriteEffects.None, 1);
+                sprite.End();
+            }
+                effect.CurrentTechnique = effect.Techniques[0];
+
+            
+
+            // ************************************* //
+
+            base.Draw(gameTime);
+        }
+    }
+}
+
