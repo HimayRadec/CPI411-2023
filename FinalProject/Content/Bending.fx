7ef3dcdd199b42af6816e88f0fa4393f863a81f0
HimayRadec <80723658+HimayRadec@users.noreply.github.com>
2023-04-18 03:13:36 -0700

    Worked on FinalProject

---
+++ FinalProject/Content/Bending.fx
@@ -0,0 +1,95 @@
+﻿matrix WorldViewProjection;
+
+struct VertexShaderInput
+{
+	float4 Position : POSITION0;
+	float4 Color : COLOR0;
+	// pass in worldPos?
+};
+
+struct VertexShaderOutput
+{
+	float4 Position : SV_POSITION;
+	float4 Color : COLOR0;
+};
+
+// Functions Used for Wave Generation
+float4 SmoothCurve(float4 x)
+{
+    return x * x * (3.0 - 2.0 * x);
+}
+
+
+float4 TriangleWave(float4 x)
+{
+    return abs(frac(x + 0.5) * 2.0 - 1.0);
+}
+
+float4 SmoothTriangleWave(float4 x)
+{
+    return SmoothCurve(TriangleWave(x));
+}
+// DETAIL BENDING
+// Leave bending is done by deforming the edges using the vertex red value for edge stiffness
+// done along world space using the winds xy direction
+
+// per leaf is done by deforming along the z axis using the blue value 
+
+// to give each leaf its own phase variation we use the g value
+
+// Phases (object, vertex, branch)
+
+
+// MAIN BENDING
+// main vegetation bends by displacing vertices' xy positions along the wind direction, 
+// using normalized height to scale the amount of deformation
+
+// computing the vertex's distance to the mesh center and using this distance for rescaling the new displaced normalized position ????
+
+VertexShaderOutput MyVertexShader(in VertexShaderInput input)
+{
+	VertexShaderOutput output = (VertexShaderOutput)0;
+	
+	// DETAILED BENDING
+    float fObjPhase = dot(worldPos.xyz, 1);
+    fBranchPhase += fObjPhase; // what is fBranchPhase (main vegetation)?
+	//  x is used for edges; y is used for branches
+    float fVtxPhase = dot(vPos.xyz, fDetailPhase + fBranchPhase); // what is fDetailPhase
+	
+    float2 vWavesIn = fTime + float2(fVtxPhase, fBranchPhase); // 1.975, 0.793, 0.375, 0.193 are good frequencies
+    float4 vWaves = (frac(vWavesIn.xxyy * float4(1.975, 0.793, 0.375, 0.193)) * 2.0 - 1.0) * fSpeed * fDetailFreq;
+    vWaves = SmoothTriangleWave(vWaves);
+    float2 vWavesSum = vWaves.xz + vWaves.yw; // Edge (xy) and branch bending (z)
+    vPos.xyz += vWavesSum.xxy * float3(fEdgeAtten * fDetailAmp * vNormal.xy, fBranchAtten * fBranchAmp);
+	
+	
+	
+	// MAIN BENDING
+    float fBF = vPos.z * fBendScale; // Smooth bending factor and increase its nearby height limit.
+    fBF += 1.0;
+    fBF *= fBF;
+    fBF = fBF * fBF - fBF; // Displace position
+    float3 vNewPos = vPos;
+    vNewPos.xy += vWind.xy * fBF; // Rescale 
+	vPos.xyz = normalize(vNewPos.xyz)* fLength;
+	
+
+	output.Position = mul(input.Position, WorldViewProjection);
+	output.Color = input.Color;
+
+	return output;
+}
+
+float4 MyPixelShader(VertexShaderOutput input) : COLOR
+{
+	return input.Color;
+}
+
+technique BasicColorDrawing
+{
+	pass P0
+	{
+        VertexShader = compile vs_4_0 MyVertexShader();
+        PixelShader = compile ps_4_0 MyPixelShader();
+    }
+};
---
+++ FinalProject/FinalProject.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace FinalProject
+{
+    public class FinalProject : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        // **** TEMPLATE ************//
+        SpriteFont font;
+        Effect effect;
+        Matrix world = Matrix.Identity;
+        Matrix view = Matrix.CreateLookAt(new Vector3(20, 0, 0), new Vector3(0, 0, 0), Vector3.UnitY);
+        Matrix projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45), 800f / 600f, 0.1f, 100f);
+        Vector3 cameraPosition, cameraTarget, lightPosition;
+        Matrix lightView, lightProjection;
+        float angle = 0;
+        float angle2 = 0;
+        float angleL = 0;
+        float angleL2 = 0;
+        float distance = 10;
+        MouseState preMouse;
+        Model model;
+        Model[] models;
+        Texture2D texture;
+        // **** TEMPLATE ************//
+
+        public FinalProject()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+
+            _graphics.GraphicsProfile = GraphicsProfile.HiDef;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            // ************ TEMPLATE ************ //
+            if (Keyboard.GetState().IsKeyDown(Keys.Left)) angleL += 0.02f;
+            if (Keyboard.GetState().IsKeyDown(Keys.Right)) angleL -= 0.02f;
+            if (Keyboard.GetState().IsKeyDown(Keys.Up)) angleL2 += 0.02f;
+            if (Keyboard.GetState().IsKeyDown(Keys.Down)) angleL2 -= 0.02f;
+            if (Keyboard.GetState().IsKeyDown(Keys.S)) { angle = angle2 = angleL = angleL2 = 0; distance = 30; cameraTarget = Vector3.Zero; }
+            if (Mouse.GetState().LeftButton == ButtonState.Pressed)
+            {
+                angle -= (Mouse.GetState().X - preMouse.X) / 100f;
+                angle2 += (Mouse.GetState().Y - preMouse.Y) / 100f;
+            }
+            if (Mouse.GetState().RightButton == ButtonState.Pressed)
+            {
+                distance += (Mouse.GetState().X - preMouse.X) / 100f;
+            }
+
+            if (Mouse.GetState().MiddleButton == ButtonState.Pressed)
+            {
+                Vector3 ViewRight = Vector3.Transform(Vector3.UnitX,
+                    Matrix.CreateRotationX(angle2) * Matrix.CreateRotationY(angle));
+                Vector3 ViewUp = Vector3.Transform(Vector3.UnitY,
+                    Matrix.CreateRotationX(angle2) * Matrix.CreateRotationY(angle));
+                cameraTarget -= ViewRight * (Mouse.GetState().X - preMouse.X) / 10f;
+                cameraTarget += ViewUp * (Mouse.GetState().Y - preMouse.Y) / 10f;
+            }
+            preMouse = Mouse.GetState();
+            // Update Camera
+            cameraPosition = Vector3.Transform(new Vector3(0, 0, distance),
+                Matrix.CreateRotationX(angle2) * Matrix.CreateRotationY(angle) * Matrix.CreateTranslation(cameraTarget));
+            view = Matrix.CreateLookAt(
+                cameraPosition,
+                cameraTarget,
+                Vector3.Transform(Vector3.UnitY, Matrix.CreateRotationX(angle2) * Matrix.CreateRotationY(angle)));
+
+            // Update Light
+            lightPosition = Vector3.Transform(
+                new Vector3(0, 0, 10),
+                Matrix.CreateRotationX(angleL2) * Matrix.CreateRotationY(angleL));
+
+            lightView = Matrix.CreateLookAt(
+                lightPosition,
+                Vector3.Zero,
+                Vector3.Transform(
+                    Vector3.UnitY,
+                    Matrix.CreateRotationX(angleL2) * Matrix.CreateRotationY(angleL)));
+            lightProjection =
+                Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver2, 1f, 1f, 50f);
+            // ********************************** //
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
